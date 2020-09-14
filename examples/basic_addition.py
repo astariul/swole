51c0d749bac42032ffad167d7aabd2e15888c101
colanim <nicolas.remond@42maru.com>
2020-09-14 09:06:38 +0900

    :card_file_box: Update example

--- examples/basic_addition.py
+++ examples/basic_addition.py
@@ -1,11 +1,23 @@
-from swole import Application
+from swole import Application, Ajax
 from swole.widgets import Input, Button, Markdown
 
 
+a = Input()
+b = Input()
+m = Markdown("Result : ")
+
+def addition(a, b):
+    m.content = "Result : {}".format(a + b)
+
+aj = Ajax(addition, [a, b])
+
+
+app = Application()
+app.pages['/'].add(a)
+app.pages['/'].add(b)
+app.pages['/'].add(Button("Compute", onclick=aj))
+app.pages['/'].add(m)
+
+
 if __name__ == "__main__":
-    app = Application()
-    app.pages['/'].add(Input())
-    app.pages['/'].add(Input())
-    app.pages['/'].add(Button("Compute"))
-    app.pages['/'].add(Markdown("Result : "))
     app.serve()
