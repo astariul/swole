4b28cb3d8fd75de27c22f74670fabcee626bdfe5
Astariul <remondnicola@gmail.com>
2020-10-02 20:27:47 +0900

    :package: Change package version to 0.0.2

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ with open('requirements.txt') as fr:
 
 setuptools.setup(
     name="swole",
-    version="0.0.1",
+    version="0.0.2",
     author="Nicolas REMOND",
     author_email="remondnicola@gmail.com",
     description="Streamlit, but better.",
